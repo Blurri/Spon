076301dc6a2242e67ee5b6278128cbdead13e5bf
Blurri <gaborraz@hotmail.com>
2014-04-09 22:36:44 +0200

    console log node_env

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var express = require('express'),
 if (process.env.NODE_ENV == undefined){
 	process.env.NODE_ENV = 'test';	
 }
-
+console.log(process.env.NODE_ENV);
 
 var dbUrl = config[process.env.NODE_ENV].db;
 
