e89676767d62358e791208f1a6f6b82b48d402e9
Blurri <gaborraz@hotmail.com>
2014-04-09 22:43:40 +0200

    socket.io for heroku url

--- client/js/controllers/events.js
+++ client/js/controllers/events.js
@@ -1,7 +1,7 @@
 /*
 Dependencies
 */
-var socket = io.connect('http://localhost');
+var socket = io.connect('http://spontanious.herokuapp.com/');
 var newTemplate = require('../../views/newEvent.hbs');	
 var detailTemplate = require('../../views/detailEvent.hbs');
 var Handlebars = require('handlebars');
